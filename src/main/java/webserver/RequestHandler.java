7ab2033dfe1c6615192663a5db5e8cd9da29dba6
jinny-l <jinny.l.dev@gmail.com>
2023-05-29 17:46:36 +0900

    ✨feat(3.0): 다양한 컨텐츠 타입을 지원하는 기능 구현

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -2,22 +2,24 @@ package webserver;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Map;
 import model.User;
 import model.http.request.HttpRequest;
 import model.http.request.HttpRequestBuilder;
+import model.http.response.HttpResponse;
+import model.http.response.HttpResponseBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import user.UserController;
 import user.UserService;
+import view.ViewResolver;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
@@ -32,14 +34,16 @@ public class RequestHandler implements Runnable {
     }
 
     public void run() {
-        logger.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+        logger.debug("New Client Connect! Connected IP : {}, Port : {}",
+                connection.getInetAddress(),
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            DataOutputStream dos = new DataOutputStream(out);
 
             HttpRequest httpRequest = HttpRequestBuilder.build(br);
-            logger.debug("Request: {}", httpRequest);
+            logger.debug("\nRequest: {}", httpRequest);
 
             String url = httpRequest.getUrl();
 
@@ -52,33 +56,32 @@ public class RequestHandler implements Runnable {
                 url = "/index.html";
             }
 
-            // RequestTarget 출력
-            DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = Files.readAllBytes(
-                    new File("src/main/resources/templates" + url).toPath() // TODO: 경로 별도로 추출
-            );
+            // View 찾기
+            String view = ViewResolver.getViewFrom(Paths.get(url));
+            logger.debug("View: {}", view);
 
-            response200Header(dos, body.length);
-            responseBody(dos, body);
+            // Response 생성
+            HttpResponse httpResponse = HttpResponseBuilder.build(new HttpResponse(), view, 200, "OK");
+
+            writeResponse(dos, httpResponse);
+            logger.debug("\nResponse: {}", httpResponse);
+
         } catch (IOException e) {
             logger.error(e.getMessage());
         }
     }
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+    private void writeResponse(DataOutputStream dos, HttpResponse httpResponse) {
         try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            logger.error(e.getMessage());
-        }
-    }
+            dos.writeBytes(httpResponse.getStatusLine().toString());
+            dos.writeBytes(System.lineSeparator());
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
+            dos.writeBytes(httpResponse.getResponseHeader().toString());
+            dos.writeBytes(System.lineSeparator());
+            dos.writeBytes(System.lineSeparator());
+
+            dos.write(httpResponse.getBody(), 0, httpResponse.getBody().length);
+
             dos.flush();
         } catch (IOException e) {
             logger.error(e.getMessage());
