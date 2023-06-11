362447d339b26ecf028399be5051810305263676
jinny-l <jinny.l.dev@gmail.com>
2023-06-11 17:18:16 +0900

    ✨feat(4.0): RequestHandler에 4.0 변경사항 모두 반영

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -6,31 +6,23 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Paths;
-import java.util.Map;
-import model.User;
 import model.http.request.HttpRequest;
 import model.http.request.HttpRequestBuilder;
 import model.http.response.HttpResponse;
-import model.http.response.HttpResponseBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import user.UserController;
-import user.UserService;
-import view.ViewResolver;
+import servlet.DispatcherServlet;
 
 public class RequestHandler implements Runnable {
     private static final Logger logger = LoggerFactory.getLogger(RequestHandler.class);
-
-    private final UserController userController;
 
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
-        userController = new UserController(new UserService());
     }
 
     public void run() {
@@ -45,28 +37,15 @@ public class RequestHandler implements Runnable {
             HttpRequest httpRequest = HttpRequestBuilder.build(br);
             logger.debug("\nRequest: {}", httpRequest);
 
-            String url = httpRequest.getUrl();
-
-            if (url.startsWith("/user/create")) {
-                Map<String, String> params = httpRequest.getParams();
-                User user = new User(
-                        params.get("userId"), params.get("password"),
-                        params.get("name"), params.get("email"));
-                logger.debug("User: {}", user);
-                url = "/index.html";
-            }
-
-            // View 찾기
-            String view = ViewResolver.getViewFrom(Paths.get(url));
-            logger.debug("View: {}", view);
-
-            // Response 생성
-            HttpResponse httpResponse = HttpResponseBuilder.build(new HttpResponse(), view, 200, "OK");
+            HttpResponse httpResponse = new HttpResponse();
+            DispatcherServlet dispatcherServlet = new DispatcherServlet();
+            dispatcherServlet.doDispatch(httpRequest, httpResponse);
 
             writeResponse(dos, httpResponse);
             logger.debug("\nResponse: {}", httpResponse);
 
-        } catch (IOException e) {
+        } catch (IOException | NoSuchMethodException | InvocationTargetException | InstantiationException |
+                 IllegalAccessException e) {
             logger.error(e.getMessage());
         }
     }
@@ -76,7 +55,7 @@ public class RequestHandler implements Runnable {
             dos.writeBytes(httpResponse.getStatusLine().toString());
             dos.writeBytes(System.lineSeparator());
 
-            dos.writeBytes(httpResponse.getResponseHeader().toString());
+            dos.writeBytes(httpResponse.getHeader().toString());
             dos.writeBytes(System.lineSeparator());
             dos.writeBytes(System.lineSeparator());
 
